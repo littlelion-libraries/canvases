912ceea26dbc9bed8c02d39bb762f89d0aa7ab87
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-08 22:09:00 +0700

    1.1.0

--- CanvasHashCode.cs
+++ CanvasHashCode.cs
@@ -2,8 +2,8 @@
 {
     public static class CanvasHashCode
     {
-        public const int Back = -100;
-        public const int Front = 100;
-        public const int Middle = 0;
+        public const int Back = 0;
+        public const int Front = 2;
+        public const int Middle = 1;
     }
 }
